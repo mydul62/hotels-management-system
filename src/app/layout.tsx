b4104055aa2bd0e5d97bf24ff1b1418768c63b80
unknown <mydulcse62.niter@gmail.com>
2024-09-24 10:47:29 +0600

    create navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import navbar from "@/app/components/Navbar"
+import Navbar from "@/app/components/Navbar";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <Navbar></Navbar>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-    <h1 className="text-center mt-20  uppercase  font-bold font-mono text-7xl text-black">mydul islam</h1>
-    </div>
+      <main>
+        <section className="flex flex-col items-center justify-center h-screen">
+
+        </section>
+      </main>
   );
 }
